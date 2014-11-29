e8664b76a3475d12531a08a6b6c0574a09c8cbcc
theTechie <gagan.blr@gmail.com>
2014-11-29 23:59:56 +0530

    modified provisioner.js to be static-provisioner

--- provisioner.js
+++ provisioner.js
@@ -1,14 +1,33 @@
 var Q = require('q'),
-    _ = require('underscore'),
-    SQS = require('./sqs'),
-    EC2 = require('./ec2'),
-    QUEUE_NAME = 'CS553',
-    QUEUE_LENGTH = 0,
-    MAX_INSTANCE_COUNT = 1;
+    EC2 = require('./ec2');
 
-//NOTE : Monitor Queue Length using setInterval() and provision new workers dynamically
+var argv = require('optimist')
+    .usage('Usage: $0 -w [WORKER_COUNT] -i [IDLE_TIME_SECONDS]')
+    .demand(['w'])
+    .alias('w', 'workers')
+    .describe('w', 'Number of Workers')
+    .alias('i', 'idleTime')
+    .describe('i', 'Idle Time in Seconds')
+    .default('i', '0')
+    .argv;
 
-var USER_DATA = 'I2Nsb3VkLWNvbmZpZw0KcnVuY21kOg0KIC0gWyBjZCwgIiRIT01FIiBdDQogLSBbIGdpdCwgY2xvbmUsICJodHRwczovL2dpdGh1Yi5jb20vdGhlVGVjaGllL2F3cy1ub2RlLXRhc2tlci5naXQiLCAiYXdzLW5vZGUtdGFza2VyIiBdDQogLSBbIGNkLCAiYXdzLW5vZGUtdGFza2VyIiBdDQogLSBbIG5wbSwgaW5zdGFsbCBdDQogLSBbIG5vZGVqcywgIndvcmtlci5qcyIsICItaSIsIDUgXQ0KZmluYWxfbWVzc2FnZTogIlRoZSBzeXN0ZW0gaXMgZmluYWxseSB1cCwgYWZ0ZXIgJFVQVElNRSBzZWNvbmRzIg==';
+if (parseInt(argv.workers) != argv.workers) {
+    console.log("Please provide valid value for worker count.");
+    console.log("Usage: $0 -w [WORKER_COUNT] -i [IDLE_TIME_SECONDS]");
+    process.exit();
+}
+
+if (parseInt(argv.idleTime) != argv.idleTime) {
+    console.log("Please provide valid value for idle time (0 or 5).");
+    console.log("Usage: $0 -w [WORKER_COUNT] -i [IDLE_TIME_SECONDS]");
+    process.exit();
+} else if (!(parseInt(argv.idleTime) == 0 || parseInt(argv.idleTime) == 5)) {
+    console.log("Please provide valid value for idle time (0 or 5).");
+    console.log("Usage: $0 -w [WORKER_COUNT] -i [IDLE_TIME_SECONDS]");
+    process.exit();
+}
+
+var USER_DATA_5s_IDLE_TIME = 'I2Nsb3VkLWNvbmZpZw0KcnVuY21kOg0KIC0gWyBjZCwgIiRIT01FIiBdDQogLSBbIGdpdCwgY2xvbmUsICJodHRwczovL2dpdGh1Yi5jb20vdGhlVGVjaGllL2F3cy1ub2RlLXRhc2tlci5naXQiLCAiYXdzLW5vZGUtdGFza2VyIiBdDQogLSBbIGNkLCAiYXdzLW5vZGUtdGFza2VyIiBdDQogLSBbIG5wbSwgaW5zdGFsbCBdDQogLSBbIG5vZGVqcywgIndvcmtlci5qcyIsICItaSIsIDUgXQ0KZmluYWxfbWVzc2FnZTogIlRoZSBzeXN0ZW0gaXMgZmluYWxseSB1cCwgYWZ0ZXIgJFVQVElNRSBzZWNvbmRzIg==';
 
 /*#cloud-config
 runcmd:
@@ -25,37 +44,35 @@ final_message: "The system is finally up, after $UPTIME seconds"*/
     console.error(error);
 });*/
 
-// NOTE: Check for tasks on Master Q every 1 second
-setInterval(function () {
-    SQS.getQueueLength(QUEUE_NAME).then(function (length) {
-        console.log("Queue Length:", length);
+var USER_DATA_0s_IDLE_TIME = 'I2Nsb3VkLWNvbmZpZw0KcnVuY21kOg0KIC0gWyBjZCwgIiRIT01FIiBdDQogLSBbIGdpdCwgY2xvbmUsICJodHRwczovL2dpdGh1Yi5jb20vdGhlVGVjaGllL2F3cy1ub2RlLXRhc2tlci5naXQiLCAiYXdzLW5vZGUtdGFza2VyIiBdDQogLSBbIGNkLCAiYXdzLW5vZGUtdGFza2VyIiBdDQogLSBbIG5wbSwgaW5zdGFsbCBdDQogLSBbIG5vZGVqcywgIndvcmtlci5qcyIsICItaSIsIDAgXQ0KZmluYWxfbWVzc2FnZTogIlRoZSBzeXN0ZW0gaXMgZmluYWxseSB1cCwgYWZ0ZXIgJFVQVElNRSBzZWNvbmRzIg==';
 
-        // NOTE: get number of spot instances in 'pending' and 'running' state
-        EC2.describeInstances().then(function (data) {
-            var instanceCount = data.Reservations.length;
+/*#cloud-config
+runcmd:
+ - [ cd, "$HOME" ]
+ - [ git, clone, "https://github.com/theTechie/aws-node-tasker.git", "aws-node-tasker" ]
+ - [ cd, "aws-node-tasker" ]
+ - [ npm, install ]
+ - [ nodejs, "worker.js", "-i", 0 ]
+final_message: "The system is finally up, after $UPTIME seconds"*/
 
-            console.log("[Provisioner] : Instance Count : ", instanceCount);
+var USER_DATA;
 
-            // NOTE: if the number of tasks increased, start a worker
-            if (length > QUEUE_LENGTH && instanceCount < MAX_INSTANCE_COUNT) {
-                EC2.createSpotInstances(1, USER_DATA).then(function (data) {
-                    console.log("[Provisioner] : Provisioned 1 Spot Instance.");
-                }, function (error) {
-                    console.error("[Provisioner Error -> createSpotInstances()] : " + error);
-                });
-            }
-        }, function (err) {
-            console.log("[Provisioner Error -> describeInstances()] : " + err);
-        });
-    }, function (error) {
-        console.log("[Provisioner Error -> getQueueLength()] : " + error);
-    });
-}, 1000);
+if (argv.idleTime == 0) {
+    USER_DATA = USER_DATA_0s_IDLE_TIME;
+} else {
+    USER_DATA = USER_DATA_5s_IDLE_TIME;
+}
 
-/*EC2.createSpotInstances(1, USER_DATA).then(function (data) {
-    console.log("[Provisioner] : Provisioned 1 Spot Instance.", data);
+EC2.createInstances(argv.workers, USER_DATA).then(function (data) {
+    console.log("[Provisioner] : Provisioned " + argv.workers + " On-Demand Instances.");
 }, function (error) {
-    console.error(error);
+    console.error("[Provisioner] : Error -> createSpotInstances() : ", error);
+});
+
+/*EC2.createSpotInstances(1, USER_DATA_0s_IDLE_TIME).then(function (data) {
+    console.log("[Provisioner] : Provisioned 1 Spot Instance.");
+}, function (error) {
+    console.error("[Provisioner Error -> createSpotInstances()] : " + error);
 });*/
 
 // provisioner exited
